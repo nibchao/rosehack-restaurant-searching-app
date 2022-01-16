e4e5cfd4283ba4a3b1b2a02168047f3918715458
Nicholas Chao <57279548+nibchao@users.noreply.github.com>
2022-01-15 18:09:50 -0800

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,46 +1,70 @@
 import 'package:flutter/material.dart';
-/*
-class _HomePageState extends State<HomeScreen> {
-  static const historyLength = 5;
 
-  List<String> _searchHistory = [
-    'fuchsia',
-    'flutter',
-    'widgets',
-    'resocoder'
-  ];
+void main() => runApp(MyApp());
 
-  List<String> filteredSearchHistory;
+class _HomeScreenState extends State<HomeScreen> {
+  Widget customSearchBar = const Text('Restaurant Selector');
+  Widget searchDescription = const Text('Enter your address and select a search radius');
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: customSearchBar,
+        automaticallyImplyLeading: false,
+        centerTitle: true
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: ElevatedButton (
+              child: Text('Search for Restaurants'),
+              onPressed: () => Navigator.of(context).push(MaterialPageRoute(builder: (_) => const SearchPage())),
+            )
 
-  List<String> filterSearchTerms({
-  @required String filter,
-}) {
-    if (filter != null && filter.isNotEmpty)
-      {
-        return _searchHistory.reversed.where((term)) => term.startsWith(filter)).toList();
-      }
-    else
-      {
-        return _searchHistory.reversed.toList();
-      }
+            ),
+          ],
+      ),
+      );
   }
+}
 
-  String selectedTerm = "";
+class SearchPage extends StatelessWidget {
+  const SearchPage({Key? key}) : super( key : key );
 
   @override
   Widget build(BuildContext context)
   {
     return Scaffold(
-      //
-      body: SearchResultsListView(
-        searchTerm: null,
-      ),
+      appBar: AppBar(
+      title: Container(
+        width: double.infinity,
+        height: 40,
+        decoration: BoxDecoration(
+        color: Colors.white),
+        child: Center(
+          child: TextField(
+            decoration: InputDecoration(
+              prefixIcon: Icon(Icons.search),
+              suffixIcon: IconButton(
+                icon: Icon(Icons.clear),
+                  onPressed: () {
+
+                  },
+              ),
+              hintText: 'Search...',
+              border: InputBorder.none),
+            ),
+          ),
+        ),
+      )
     );
   }
 }
-*/
 
-void main() => runApp(MyApp());
+//import 'package:material_floating_search_bar/material_floating_search_bar.dart';
+
+//void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
@@ -56,53 +80,159 @@ class MyApp extends StatelessWidget {
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
   @override
+  //_HomePageState createState() => _HomePageState();
   _HomeScreenState createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
-  Icon customIcon = const Icon(Icons.search);
-  Widget customSearchBar = const Text('Restaurant Selector');
-  Widget searchDescription = const Text('Enter your address and select a search radius');
+/*class _HomePageState extends State<HomeScreen> {
+  static const searchHistoryLength = 5;
+
+  final List<String> _searchHistory = ['1', '2', '3', '4'];
+
+  List<String> filteredSearchHistory = [];
+  String selectedTerm = '';
+  String filter = '';
+
+  List<String> filterSearchTerms({
+  @required filter,
+}) {
+    if (filter != null && filter.isNotEmpty)
+      {
+        return _searchHistory.reversed.where((term) => term.startsWith(filter)).toList();
+    }
+    else
+      {
+        return _searchHistory.reversed.toList();
+      }
+  }
+
+  void addSearchTerm(String term)
+  {
+    if (_searchHistory.contains(term))
+      {
+        putSearchTermsFirst(term);
+        return;
+      }
+    _searchHistory.add(term);
+    if (_searchHistory.length > searchHistoryLength)
+      {
+        _searchHistory.removeRange(0, _searchHistory.length - searchHistoryLength);
+      }
+    filteredSearchHistory = filterSearchTerms(filter: null);
+  }
+
+  void deleteSearchTerm(String term)
+  {
+    _searchHistory.removeWhere((element) => element == term);
+    filteredSearchHistory = filterSearchTerms(filter: null);
+  }
+
+  void putSearchTermsFirst(String term)
+  {
+    deleteSearchTerm(term);
+    addSearchTerm(term);
+  }
+
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context)
+  {
     return Scaffold(
-      appBar: AppBar(
-        title: customSearchBar,
-        automaticallyImplyLeading: false,
-        centerTitle: true,
+      body: FloatingSearchBar(
+        controller: controller,
+        body: const FloatingSearchBarScrollNotifier(
+          child: SearchResultsListView(searchTerm: selectedTerm),
+      ),
+        transition: CircularFloatingSearchBarTransition(),
+        physics: const BouncingScrollPhysics(),
+        title: Text('Search Bar', style: Theme.of(context).textTheme.headline5),
+        hint: 'Search...',
         actions: [
-          IconButton(onPressed: () {
-            setState(() {
-              if (customIcon.icon == Icons.search)
-                {
-                  customIcon = const Icon(Icons.cancel);
-                  customSearchBar = const ListTile(
-                  leading: Icon(Icons.search, color: Colors.white, size: 28),
-                  title: TextField
-                (decoration: InputDecoration(hintText: 'Enter address', hintStyle: TextStyle(color: Colors.white, fontSize: 18, fontStyle: FontStyle.italic), border: InputBorder.none),
-                  style: TextStyle(color: Colors.white),
-                ),
-                );
-                }
-              else
-                {
-                  customIcon = const Icon(Icons.search);
-                  customSearchBar = const Text('Restaurant Selector');
-                }
-            });
-          }, icon: customIcon)
+          FloatingSearchBarAction.searchToClear(),
         ],
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Center(
-            child: searchDescription,
-          ),
-        ],
-      ),
+        onQueryChanged: (query)
+        {
+          setState(() {
+            filteredSearchHistory = filterSearchTerms(filter: query);
+          });
+        },
+        onSubmitted: (query)
+        {
+          setState(() {
+            addSearchTerm(query);
+            selectedTerm = query;
+          });
+          controller.close();
+        }, builder: (context, transition) {
+          return ClipRRect(
+            borderRadius: BorderRadius.circular(8),
+            child: const Material(
+              color: Colors.white,
+              elevation: 4,
+              child: Placeholder(
+                fallbackHeight: 200,
+              ),
+            ),
+          );
+      },
+    )
     );
+  }
+
+  late FloatingSearchBarController controller;
+
+  @override
+  void initState()
+  {
+    super.initState();
+    controller = FloatingSearchBarController();
+    filteredSearchHistory = filterSearchTerms(filter: null);
+  }
+
+  @override
+  void dispose()
+  {
+    controller.dispose();
+    super.dispose();
   }
 }
 
+class SearchResultsListView extends StatelessWidget {
+  final String searchTerm = "";
+
+  const SearchResultsListView({
+    Key key;
+    @required this.searchTerm
+}) : super(key : key);
+
+  @override
+  Widget build(BuildContext context) {
+    if (searchTerm == null)
+      {
+        return Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              const Icon(
+                Icons.search,
+                size: 64,
+              ),
+              Text(
+                'Start searching',
+                style:Theme.of(context).textTheme.headline5,
+              )
+            ],
+          )
+        );
+      }
+
+      return ListView(
+      children: List.generate(50, (index) => ListTile(title: Text('$searchTerm search result'), subtitle: Text(index.toString()),
+      ),
+      ),
+      );
+  }
+}
+
+
+*/
 
